c7e88802b1f5d4a5f51780c0e6f6dbc207e4725a
Sinval Vieira <sinvalneto01@gmail.com>
2016-10-13 17:57:40 -0300

    Adding parameter to select which version of log4j the test should use

--- gelftest.sh
+++ gelftest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #ghost=graylog2.rjocta012ahobe-88.cp.globoi.com
 
 ghost=10.132.4.138
@@ -11,6 +10,7 @@ bigstack=$2
 messages=$3
 threads=$4
 tag=$5
+log4jversion=$6
 
 
 pwd=`pwd`
@@ -21,5 +21,17 @@ echo threads: $threads
 echo messages: $messages
 echo tag: $tag
 echo bigstack: $bigstack
+echo log4jversion: $log4jversion
 
-java -Dlog4j.configurationFile=file:$pwd/src/main/resources/log4j2.xml -DXmx1G -DXms1G -Dghost=$ghost -Dta=$tag -Dtype=$type -jar target/gelftest.jar $messages $threads $bigstack
+log4jConfigFile=""
+
+if [ $log4jversion == "1" ]; then 
+	log4jConfigFile="$(pwd)/src/main/resources/log4j.xml"
+	echo $log4jConfigFile
+	java -Dlog4j.configuration=file:$log4jConfigFile -DXmx1G -DXms1G -Dghost=$ghost -Dta=$tag -Dtype=$type -jar target/gelftest.jar $messages $threads $bigstack $log4jversion
+
+else
+	log4jConfigFile="$(pwd)/src/main/resources/log4j2.xml"
+	java -Dlog4j.configurationFile=file:$log4jConfigFile -DXmx1G -DXms1G -Dghost=$ghost -Dta=$tag -Dtype=$type -jar target/gelftest.jar $messages $threads $bigstack $log4jversion
+
+fi
